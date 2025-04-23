4a5616fffa4915ddded05567eacc64db2fe809a4
Kira Vasilev <kirillwork56@gmail.com>
2025-04-24 01:58:54 +0300

    update

--- examples/test3.ts
+++ examples/test3.ts
@@ -12,3 +12,34 @@
 //     return new Config(data);
 //   }
 // }
+
+export default class HTMLElement {
+  attributes: object;
+
+  constructor(attributes = {}) {
+    this.attributes = attributes;
+  }
+
+  stringifyAttributes() {
+    // BEGIN (write your solution here)
+    let str = "";
+
+    for (const key in this.attributes) {
+      if (Object.prototype.hasOwnProperty.call(this.attributes, key)) {
+        const element = this.attributes[key];
+        str += ` ${key}="${element}"`;
+      }
+    }
+
+    return str;
+
+    // END
+  }
+}
+
+class HTMLHrElement extends HTMLElement {
+  toString() {
+    const attributes = this.stringifyAttributes();
+    return attributes ? `<hr ${attributes}>` : "<hr>";
+  }
+}
